899efe6716d935f3b0f12a15b48e4cba9bc7f869
Pekhov <pehovanton21@gmail.com>
2020-02-16 00:09:03 +0300

    insertion sort

---
+++ insertionSsort/main.py
@@ -0,0 +1,20 @@
+
+nums = [5, 2, 4, 6, 1, 3]
+
+
+def insertion_sort(nums):
+    # Сортировку начинаем со второго элемента, т.к. считается, что первый элемент уже отсортирован
+    for i in range(1, len(nums)):
+        item_to_insert = nums[i]
+        # Сохраняем ссылку на индекс предыдущего элемента
+        j = i - 1
+        # Элементы отсортированного сегмента перемещаем вперёд, если они больше
+        # элемента для вставки
+        while j >= 0 and nums[j] > item_to_insert:
+            nums[j + 1] = nums[j]
+            j -= 1
+        # Вставляем элемент
+        nums[j + 1] = item_to_insert
+
+insertion_sort(nums)
+print(nums)
